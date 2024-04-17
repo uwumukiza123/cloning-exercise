81033df7b320fad3f3d7dcdb831db7dd9448c5a1
uwumukiza123 <uwumukizalaetitia@gmail.com>
2024-04-17 09:18:49 +0200

    refactor degree program component

--- src/components/DegreePrograms.jsx
+++ src/components/DegreePrograms.jsx
@@ -4,19 +4,19 @@ import presidentialStudentLunch from '../assets/csm_20211111_Presidential_studen
 
 const DegreePrograms = () => {
   return (
-    <div className=''>
-        <div className='flex justify-between bg-gray-200 hover hover:bg-gray-100 h-16 border-b border-secondary'>
+    <div className='pl-4 sm:pl-10 pr-4 sm:pr-0'>
+        <div className='flex justify-between bg-gray-200 hover hover:bg-gray-100 h-16 border-b border-secondary mr-0 sm:mr-10'>
             <button className='px-4'>Select degree program or enter keyword</button>
             <img src={chevronDown} alt="chevron down" className='px-3'/>
         </div>
-        <div className='flex gap-2 items-center justify-end text-secondary font-semibold'>
+        <div className='flex gap-2 items-center justify-end text-secondary font-semibold mr-0 sm:mr-10'>
             <span>Degree programs</span>
             <img src={arrowRight} alt="arrow right" className='w-7'/>
         </div>
 
         <div className='block sm:flex'>
             <img src={presidentialStudentLunch} alt="presidentila student lunch" />
-            <div className='bg-secondary w-full h-96 sm:w-200 static sm:relative right-20  top-6'>
+            <div className='bg-secondary w-full h-full sm:h-80 static sm:relative right-10 top-16'>
                 <div className='p-8 text-white'>
                     <h1 className='font-bold text-3xl'>What drives us: President Thomas F. Hofmann about TUM</h1>
                     <p className='py-10'>Research and innovations for society, innovative teaching and continuing education: This is how we shape a sustainable future, relying on the intelligence of the entire university and working closely with global partners to do so</p>
